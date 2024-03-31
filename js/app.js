88fc919fb9381dee5794ef3a3783e49b219f13af
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-03-31 13:47:07 +0500

    fffee

---
+++ js/app.js
@@ -0,0 +1,82 @@
+//1-masala//
+//with class
+/*class Car {
+  constructor(brendi, yili) {
+    this.brand = brendi;
+    this.year = yili;
+  }
+}
+let type1 = new Car("Gelik", 2024);
+console.log(type1);
+
+class anothercar extends Car {
+  constructor(brendi, yili) {
+    super(brendi, yili);
+  }
+}
+let type2 = new Car("BMW", 2023);
+console.log(type2);
+class anotherone extends Car {
+  constructor(brendi, yili) {
+    super(brendi, yili);
+  }
+}
+let type3 = new Car("Bgatty", 2022);
+console.log(type3);*/
+
+//2-masala//
+/*class Auto {
+  constructor(brendi, yili, oil) {
+    this.brand = brendi;
+    this.year = yili;
+    this.powerbak = oil;
+  }
+}
+let item = new Auto("Gelik", 2024, "gas");
+console.log(item);
+
+class ElectricCar extends Auto {
+  constructor(brendi, yili, oil, el) {
+    super(brendi, yili, oil);
+    this.batteryLife = el;
+  }
+}
+let item2 = new ElectricCar("BMW", 2023, "gas", "elector");
+console.log(item2);*/
+
+//3-masala//
+/*class Car {
+  constructor(brendi, yili) {
+    this.brand = brendi;
+    this.year = yili;
+  }
+  displayInfo() {
+    console.log("Gelik 2024");
+  }
+}
+let el1 = new Car("Gelik", 2024);
+Car.displayInfo();
+console.log(el1);
+
+class anothercar extends Car {
+  constructor(brendi, yili) {
+    super(brendi, yili);
+  }
+  displayInfo() {
+    console.log("BMW 2023");
+  }
+}
+let el2 = new Car("BMW", 2023);
+anothercar.displayInfo();
+console.log(el2);*/
+
+//4-masala//
+/*let item1 = document.getElementById("city");
+
+console.log((item1.textContent = "Toshkent"));
+let item2 = document.getElementsByClassName("city");
+
+console.log((item2.textContent = "Andijon"));
+let item3 = document.getElementsByTagName("li");
+
+console.log((item3.textContent = "Namangan"));*/
